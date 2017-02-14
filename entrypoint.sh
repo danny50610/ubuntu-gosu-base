287156cae4a68ddcda6cb81d97a3a1bf1e1a136e
Chan, Danny <danny50610@yahoo.com.tw>
2017-02-14 14:52:53 +0800

    Init project

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Add local user
+# Either use the LOCAL_USER_ID if passed in at runtime or
+# fallback
+
+USER_ID=${LOCAL_USER_ID:-9001}
+
+echo "Starting with UID : $USER_ID"
+useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+export HOME=/home/user
+
+cp -r /root/.gradle /home/user
+chown -R user:user /home/user/.gradle
+
+exec /usr/local/bin/gosu user "$@"
